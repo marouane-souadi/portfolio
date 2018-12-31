de55855cfb54a64bb8b9eced9594049250bee01a
Your Name <you@example.com>
2018-12-31 16:58:18 +0100

    first version

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-
-// const sectionNames = ["home", "about", "projects", "skills"];
-// const navBtns = {}
-// const sectionEls = {}
-
-// for (let i = 0; i < sectionNames.length; i++) {
-//   navBtns[sectionNames[i]] = document.getElementById(sectionNames[i]+"-btn")
-//   sectionEls[sectionNames[i]] = document.getElementById(sectionNames[i]+"-section")
-// }
-
-// let currentIndex = sectionNames.indexOf("home")
-// navBtns[sectionNames[currentIndex]].classList.toggle("selected");
-
-// for (let j = 0; j< sectionNames.length; j++) {
-//   navBtns[sectionNames[j]].addEventListener("click", ()=>{
-//     if (j !== currentIndex) {
-//       // debugger;
-//       sectionEls[sectionNames[currentIndex]].classList.toggle("hidden");
-//       sectionEls[sectionNames[j]].classList.toggle("hidden");
-//       navBtns[sectionNames[currentIndex]].classList.toggle("selected");
-//       navBtns[sectionNames[j]].classList.toggle("selected");
-//       currentIndex = j;
-//     }
-//   })
-// }
-
 var height = document.getElementById("header").offsetHeight;
 document.getElementById("home-section").style.paddingTop = height + 'px';
 console.log(height)
